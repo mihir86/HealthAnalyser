f88077cd48cd659c86c07ab031f92d6b1a52f0cf
mihir86 <mstrmihir2@gmail.com>
2020-03-21 16:46:16 +0530

    Added questionaire

--- app/src/main/java/com/example/healthanalyser/MainActivity.java
+++ app/src/main/java/com/example/healthanalyser/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.healthanalyser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button questionaire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionaire = (Button) findViewById(R.id.buttonques);
+        questionaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Questions.class));
+            }
+        });
+
     }
+
 }
