df3c3f247117ebaebb88fcef3be8484eb5aaf5e9
swooshie <57328174+swooshie@users.noreply.github.com>
2020-03-22 15:41:50 +0530

    Add files via upload

--- app/src/main/java/com/example/healthanalyser/QuestionsLibrary.java
+++ app/src/main/java/com/example/healthanalyser/QuestionsLibrary.java
@@ -2,14 +2,43 @@ package com.example.healthanalyser;
 
 public class QuestionsLibrary {
     private String questions [] = {
-        "How are you","Your name is"
+        "In the past two weeks, how often have you felt down or hopeless?",
+        "Have you had any thoughts of suicide?",
+        "How is your sleep",
+        "How is your energy?",
+        "Do you prefer to stay at home rather than going out and doing new things?",
+        "Overall, How would you decribe your mood?",
+        "Do you consume drugs",
+        "How connected do you feel to the people around you?",
+        "Are you eating more or less than you normally do?",
+        "Are you capable of enjoying things right now?",
+        "how often do you experience physical symptoms like headaches, insomnia, or digestive issues?"
     };
     private String options[][] = {
-            {"Good","Bad","Lucky","Great"},
-            {"Gsdood","Brad","Lucsky","Greasdt"}
+            {"not at all","several days","more than half of the days","nearly every day"},
+            {"never","some thoughts of death","some thoughts of suicide","some attempt of suicide"},
+            {"sleeping as usual","slight difficulty","sleep reduced by atleast two hours","getting less than 3 hours of sleep"},
+            {"As much energy as ever","less energy than before","not enough to do much","not enough to do anything"},
+            {"yes","no"},
+            {"Like a roller coaster", "pretty much steady", "All blues" ,"cheerful"},
+            {"yes","no","sometimes"},
+            {"A lot", "Not much", "I dont talk to people"},
+            {"eating as usual", "slightly less", "skip atleast one meal", "eating less than 2 meals"},
+            {"yes", "no"}
+            {"alamost everyday", "several days", "seldom", "never"}
     };
     private int weights[][] = {
-            {10,20,30,40},{10,20,40,40}
+            {0,3,6,12},
+            {0,6,6,12},
+            {0,2,4,8},
+            {0,2,4,8},
+            {4,0},
+            {4,2,8,0},
+            {12,0,3},
+            {0,4,2},
+            {0,2,4,8},
+            {0,2},
+            {8,4,2,0}
     };
 
     public String getques(int i){
